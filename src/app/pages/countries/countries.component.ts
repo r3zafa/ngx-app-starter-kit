31dd460c052769675f0c7a3ac3a80cd16be7211e
r3zafa <r3zafaraji@gmail.com>
2025-07-17 03:29:33 +0200

    countries store and component enhenced

--- src/app/pages/countries/countries.component.ts
+++ src/app/pages/countries/countries.component.ts
@@ -1,20 +1,43 @@
-import { Component, inject } from '@angular/core';
-import {CountriesStore} from '../../stores'
+import { Component, inject, signal } from '@angular/core';
+import { CountriesStore } from '../../stores';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-countries',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatOptionModule
+  ],
   templateUrl: './countries.component.html',
   styleUrl: './countries.component.scss'
 })
 export class CountriesComponent {
- countriesStore = inject(CountriesStore);
+  readonly countriesStore = inject(CountriesStore);
 
-   onRegionChange(event: Event) {
-    const selectElement = event.target as HTMLSelectElement;
-    if (selectElement) {
-      this.countriesStore.setRegion(selectElement.value);
-    }
+  // Properly typed search handler
+  handleSearch(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.countriesStore.setSearchQuery(target.value);
+  }
+
+  // Region change handler
+  onRegionsChange(regions: string[]) {
+    this.countriesStore.setRegions(regions);
   }
 }
