88d30bbf2b46fd474a8594c939c7c7c2e752828a
r3zafa <r3zafaraji@gmail.com>
2025-08-07 01:56:17 +0200

    add "@angular/pwa" to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   ApplicationConfig,
   provideZoneChangeDetection,
-  importProvidersFrom,
+  importProvidersFrom, isDevMode,
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 import { provideHttpClient } from "@angular/common/http";
 import { translationModuleProvider } from "./shared";
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +16,9 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideRouter(routes),
     importProvidersFrom(translationModuleProvider),
-    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(BrowserAnimationsModule), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
